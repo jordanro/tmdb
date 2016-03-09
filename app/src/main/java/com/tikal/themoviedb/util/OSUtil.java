566d59421fbf4b94f753e4ba3073fe7604e73d82
Yarden Rosenberg <yarden@clyng.com>
2016-03-09 03:00:22 +0200

    fix tablet/phone layout issues

--- app/src/main/java/com/tikal/themoviedb/util/OSUtil.java
+++ app/src/main/java/com/tikal/themoviedb/util/OSUtil.java
@@ -1,11 +1,7 @@
 package com.tikal.themoviedb.util;
 
-import android.content.Context;
-import android.util.DisplayMetrics;
-import android.view.Display;
-import android.view.WindowManager;
-
 import com.tikal.themoviedb.MovieDBApplication;
+import com.tikal.themoviedb.R;
 
 /**
  * Created by Yarden Rosenberg on 08/03/2016.
@@ -13,24 +9,24 @@ import com.tikal.themoviedb.MovieDBApplication;
 public class OSUtil {
 
     public static boolean isTablet(){
-//        return MovieDBApplication.getAppContext().getResources().getBoolean(R.bool.isTablet);
+        return MovieDBApplication.getAppContext().getResources().getBoolean(R.bool.isTablet);
 //        boolean xlarge = ((MovieDBApplication.getAppContext().getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_XLARGE);
 //        boolean large = ((MovieDBApplication.getAppContext().getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
 
-        boolean isTablet = false;
-        WindowManager wm = (WindowManager) MovieDBApplication.getAppContext().getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        DisplayMetrics metrics = new DisplayMetrics();
-        display.getMetrics(metrics);
-
-        float widthInches = metrics.widthPixels / metrics.xdpi;
-        float heightInches = metrics.heightPixels / metrics.ydpi;
-        double diagonalInches = Math.sqrt(Math.pow(widthInches, 2) + Math.pow(heightInches, 2));
-        if (diagonalInches >= 7.0) {
-            isTablet = true;
-        }
-
-        return isTablet;
+//        boolean isTablet = false;
+//        WindowManager wm = (WindowManager) MovieDBApplication.getAppContext().getSystemService(Context.WINDOW_SERVICE);
+//        Display display = wm.getDefaultDisplay();
+//        DisplayMetrics metrics = new DisplayMetrics();
+//        display.getMetrics(metrics);
+//
+//        float widthInches = metrics.widthPixels / metrics.xdpi;
+//        float heightInches = metrics.heightPixels / metrics.ydpi;
+//        double diagonalInches = Math.sqrt(Math.pow(widthInches, 2) + Math.pow(heightInches, 2));
+//        if (diagonalInches >= 7.0) {
+//            isTablet = true;
+//        }
+//
+//        return isTablet;
     }
 
 }
